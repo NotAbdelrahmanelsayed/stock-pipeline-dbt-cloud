e630b404e7457879e1ea9bfea030217affcbcac5
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-02 10:46:59 +0300

    Constants file for configurations

---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+from utils.logging_config import setup_logging
+
+# Prepare the logger
+logger = setup_logging()
+
+# Tickers labels
+TICKERS = ['MSFT', 'AAPL', 'NVDA', 'AMZN', 'GOOGL', '2222.SR', 'META', 'BRK-B', 'AVGO', 'TSM']
