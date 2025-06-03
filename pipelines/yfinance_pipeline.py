b140deeb839330fef064ced9a7946b5a20fa021a
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-03 10:53:10 +0300

    stage data locally

--- pipelines/yfinance_pipeline.py
+++ pipelines/yfinance_pipeline.py
@@ -1,5 +1,5 @@
-from etl.yfinance_etl import download_stock_data, flatten_multi_index
-from utils.constants import TICKERS, logger
+from etl.yfinance_etl import download_stock_data, flatten_multi_index, save_data_locally
+from utils.constants import TICKERS, logger, RAW_DATA_FILE_PATH
 from typing import Optional, List
 import pandas as pd 
 
@@ -7,6 +7,7 @@ import pandas as pd
 def initial_download_stock_data():
     data = download_stock_data(TICKERS, is_full_load=True)
     df = flatten_multi_index(data)
+    save_data_locally(df, RAW_DATA_FILE_PATH)
     logger.info(df.head())
     
 # def incremental_download_stock_data(
