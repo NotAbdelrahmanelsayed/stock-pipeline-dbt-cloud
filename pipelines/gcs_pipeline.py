5b0d88f05cdac7668104444b4b845c087d6f2ed5
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-03 10:51:56 +0300

    upload to GCS pipeline "tested"

---
+++ pipelines/gcs_pipeline.py
@@ -0,0 +1,13 @@
+from utils.constants import SERVICE_ACC_FILE, BUCKET_NAME, RAW_DATA_FILE_PATH, GSC_RAW_DATA_PATH, logger
+from etl.gcs_etl import initialize_gcs_client, upload_blob
+
+def upload_to_gcs():
+    try:
+        # Initialze google cloud storage client
+        client = initialize_gcs_client(SERVICE_ACC_FILE)
+        # Upload the data to google cloud storage
+        upload_blob(client, BUCKET_NAME, RAW_DATA_FILE_PATH, GSC_RAW_DATA_PATH)
+        logger.info(f"File {GSC_RAW_DATA_PATH} successfully uploaded to GSC from airflow")
+    except Exception as e:
+        logger.error(f"Airflow GSC upload failed :{e}")
+        raise
