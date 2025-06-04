ffd29114409bc14bd6f841f46956bbea804f363c
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-04 21:25:28 +0300

    add delta_download function

--- pipelines/yfinance_pipeline.py
+++ pipelines/yfinance_pipeline.py
@@ -8,11 +8,15 @@ def initial_download_stock_data():
     data = download_stock_data(TICKERS, is_full_load=True)
     df = transform(data)
     save_data_locally(df, RAW_DATA_FILE_PATH)
-    logger.info(df.head())
-    
-# def incremental_download_stock_data(
-# tickers: List[str], 
-# start_date: Optional[str] = None, 
-# end_date: Optional[str] = None, 
-# is_full_load: Optional[bool] = True):
-#     pass
+    logger.info(f"data saved successfully into {RAW_DATA_FILE_PATH}.")
+
+
+def delta_download_stock_data(**kwargs):
+    ti = kwargs['ti']
+    max_date = ti.xcom_pull(task_ids='decide_load_type', key='max_date')
+
+    data = download_stock_data(TICKERS, is_full_load=False, start_date=max_date)
+    df = transform(data)
+#################################### THIS FUNCTION SHOULD USE GENERATE FILE_NAME ####################################
+    save_data_locally(df, RAW_DATA_FILE_PATH)
+    logger.info(f"data saved successfully into {RAW_DATA_FILE_PATH}.")
