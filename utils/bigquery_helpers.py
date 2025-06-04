8b8e4cb42b61c7c0f431c1dfb3f466394c4948c1
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-04 21:26:14 +0300

    add xcom to decide load type

--- utils/bigquery_helpers.py
+++ utils/bigquery_helpers.py
@@ -1,8 +1,9 @@
 from google.cloud import bigquery
 from typing import Union
+from utils.constants import logger
 
 
-def get_last_loaded_date(client: bigquery.Client, table_id) -> Union[str, None]:
+def get_last_loaded_date(client: bigquery.Client, table_id, **kwargs) -> Union[str, None]:
     """Get the maximum date in BigQuery table
     Parameters
     ----------
@@ -17,13 +18,20 @@ def get_last_loaded_date(client: bigquery.Client, table_id) -> Union[str, None]:
         date as a string or None
     """
     ""
+    ti = kwargs['ti']
     QUERY = f"SELECT MAX(Date) FROM {table_id}"
     try:
+        logger.info(f"Connecting to {table_id}....")
         results = client.query_and_wait(QUERY)
         max_date = None 
         for row in results:
             max_date = row[0]
-        return max_date
+        if max_date:
+            logger.info(f"Last date from {table_id}: {max_date}\n")
+            logger.info(f"Delta load activated....")
+            ti.xcom_push(key='max_date', value=max_date)
+        return "extract_stock_data_delta" 
     except Exception as e:
-        "max_date is not"
-        return None
+        logger.info(f"Table {table_id} Not found or empty")
+        logger.info(f"Initial load activated....")
+        return "extract_stock_data_full"
