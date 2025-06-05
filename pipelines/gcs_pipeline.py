7baf155dae4b49426056fe07d398d09f81248e82
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-05 12:21:14 +0300

    file_name logic

--- pipelines/gcs_pipeline.py
+++ pipelines/gcs_pipeline.py
@@ -2,21 +2,19 @@ from utils.constants import SERVICE_ACC_FILE, BUCKET_NAME, RAW_DATA_FILE_PATH, G
 from etl.gcs_etl import initialize_gcs_client, upload_blob
 
 def upload_to_gcs(**kwargs) -> str:
-    """upload data to BigQuery
-
-    local_data -> GCS -> BigQuery
-    """
+    """Upload local data to Google Cloud Storage."""
     try:
         ti = kwargs['ti']
-        # Initialze google cloud storage client
+        file_path = ti.xcom_pull(task_ids='extract_stock_data_delta', key='file_name')
+
+        # Initialize google cloud storage client
         gcs_client = initialize_gcs_client(SERVICE_ACC_FILE)
         # Upload the data to google cloud storage
-        data_uri = upload_blob(gcs_client, BUCKET_NAME, RAW_DATA_FILE_PATH, GSC_RAW_DATA_PATH)
-        logger.info(f"File {GSC_RAW_DATA_PATH} successfully uploaded to GSC from airflow")
+        data_uri = upload_blob(gcs_client, BUCKET_NAME, file_path, file_path)
+        logger.info(f"File {file_path} successfully uploaded to GSC from airflow")
         
         # Push data_uri to Xcom
         ti.xcom_push(key='data_uri', value=data_uri)
     except Exception as e:
         logger.error(f"Airflow GSC upload failed :{e}")
         raise
-    
--- pipelines/yfinance_pipeline.py
+++ pipelines/yfinance_pipeline.py
@@ -1,5 +1,6 @@
 from etl.yfinance_etl import download_stock_data, transform, save_data_locally
 from utils.constants import TICKERS, logger, RAW_DATA_FILE_PATH
+from utils.filename_utils import generate_filename
 from typing import Optional, List
 import pandas as pd 
 
@@ -7,8 +8,9 @@ import pandas as pd
 def initial_download_stock_data():
     data = download_stock_data(TICKERS, is_full_load=True)
     df = transform(data)
-    save_data_locally(df, RAW_DATA_FILE_PATH)
-    logger.info(f"data saved successfully into {RAW_DATA_FILE_PATH}.")
+    file_name = generate_filename()
+    save_data_locally(df, file_name)
+    logger.info(f"data saved successfully into {file_name}.")
 
 
 def delta_download_stock_data(**kwargs):
@@ -17,6 +19,10 @@ def delta_download_stock_data(**kwargs):
 
     data = download_stock_data(TICKERS, is_full_load=False, start_date=max_date)
     df = transform(data)
-#################################### THIS FUNCTION SHOULD USE GENERATE FILE_NAME ####################################
-    save_data_locally(df, RAW_DATA_FILE_PATH)
-    logger.info(f"data saved successfully into {RAW_DATA_FILE_PATH}.")
+
+    file_name = generate_filename(max_date)
+    save_data_locally(df, file_name)
+    logger.info(f"data saved successfully into {file_name}.")
+    
+    # Push the file_name 
+    ti.xcom_push(key='file_name', value=file_name)
