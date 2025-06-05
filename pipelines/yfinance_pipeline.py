1255d90ef893e36002f47bf814b967e5055b48fa
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-05 20:51:16 +0300

    adding xcom filename handeling

--- pipelines/yfinance_pipeline.py
+++ pipelines/yfinance_pipeline.py
@@ -1,16 +1,13 @@
 from etl.yfinance_etl import download_stock_data, transform, save_data_locally
-from utils.constants import TICKERS, logger, RAW_DATA_FILE_PATH
-from utils.filename_utils import generate_filename
-from typing import Optional, List
-import pandas as pd 
+from utils.constants import TICKERS, logger, get_local_file_path
 
 
 def initial_download_stock_data():
     data = download_stock_data(TICKERS, is_full_load=True)
     df = transform(data)
-    file_name = generate_filename()
-    save_data_locally(df, file_name)
-    logger.info(f"data saved successfully into {file_name}.")
+    file_path = get_local_file_path()
+    save_data_locally(df, file_path)
+    logger.info(f"data saved successfully into {file_path}.")
 
 
 def delta_download_stock_data(**kwargs):
@@ -20,9 +17,9 @@ def delta_download_stock_data(**kwargs):
     data = download_stock_data(TICKERS, is_full_load=False, start_date=max_date)
     df = transform(data)
 
-    file_name = generate_filename(max_date)
-    save_data_locally(df, file_name)
-    logger.info(f"data saved successfully into {file_name}.")
-    
-    # Push the file_name 
-    ti.xcom_push(key='file_name', value=file_name)
+    file_path = get_local_file_path(max_date)
+    save_data_locally(df, file_path)
+    logger.info(f"data saved successfully into {file_path}.")
+
+    # Push the file_path
+    ti.xcom_push(key='file_name', value=file_path)
