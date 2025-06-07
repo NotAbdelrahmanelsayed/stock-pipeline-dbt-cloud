16a8f9f2367cc77695f483a0497fd522ceabd5cc
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-07 11:39:21 +0300

    dbt: stg_model and it's schema

--- stock_modeling/models/staging/stg_stock_analysis.sql
+++ stock_modeling/models/staging/stg_stock_analysis.sql
@@ -1,6 +1,15 @@
 SELECT 
-    *
+    Date AS trade_date,
+    Ticker AS ticker,
+    Open AS open_price,
+    High AS high_price,
+    low AS low_price,
+    Close AS close_price,
+    Volume AS volume
+
 FROM 
     {{ source('stock_data', 'raw_stock_prices') }}
+WHERE
+    Ticker IS NOT NULL
 
-LIMIT 10;
+limit 10
