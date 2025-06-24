ad2674d09b56481006a858e9059457c42d8f9b07
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-06-24 11:09:07 +0300

    feat(dbt): calculate daily/weekly return and rolling volatility

---
+++ stock_modeling/models/facts/fact_stock_returns.sql
@@ -0,0 +1,39 @@
+WITH base AS (
+    SELECT 
+        trade_date,
+        ticker,
+        close_price,
+        LAG(close_price) OVER (
+            PARTITION BY ticker
+            ORDER BY trade_date
+        ) AS prev_day_close_price
+    FROM 
+        {{ ref("stg_stock_prices") }}
+    
+),
+
+returns AS (
+    SELECT 
+    trade_date,
+    ticker,
+    close_price,
+    SAFE_DIVIDE(close_price - prev_day_close_price, prev_day_close_price) AS daily_return,
+FROM
+    base
+)
+
+SELECT 
+    *,
+    AVG(daily_return) OVER (
+        PARTITION BY ticker
+        ORDER BY trade_date
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS rolling_return_7d,
+    STDDEV(daily_return) OVER(
+        PARTITION BY ticker
+        ORDER BY trade_date
+        ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+        ) AS rolling_volatility_30d
+
+FROM 
+    returns
